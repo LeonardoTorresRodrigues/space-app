a1485a4de94ea88c8fbd2d2e6deca3a38bda7387
Leonardo Torres Rodrigues <leonardo.t.rodri@gmail.com>
2025-01-14 15:29:21 -0300

    feat: add gallery section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Cabecalho from "./components/Cabecalho"
 import BarraLateral from "./components/BarraLateral"
 import Banner from "./components/Banner"
 import bannerBackground from './assets/banner.png'
+import Galeria from "./components/Galeria"
 
 const FundoGradiente = styled.div`
   background: linear-gradient(172.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
@@ -11,15 +12,21 @@ const FundoGradiente = styled.div`
   min-height: 100vh;
 `
 
+const AppContainer = styled.div`
+  width: 1440px;
+  margin: 0 auto;
+  max-width: 100%;
+`
+
 const MainContainer = styled.main`
   display: flex;
   gap: 24px;
 `
 
-const AppContainer = styled.div`
-  width: 1440px;
-  margin: 0 auto;
-  max-width: 100%;
+const ConteudoGaleria = styled.section`
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
 `
 
 function App() {
@@ -30,10 +37,13 @@ function App() {
         <Cabecalho />
         <MainContainer>
           <BarraLateral />
-          <Banner 
-            texto='A galeria mais completa de fotos do espaço!' 
-            backgroundImage={bannerBackground}
-          />
+          <ConteudoGaleria>
+            <Banner 
+              texto='A galeria mais completa de fotos do espaço!' 
+              backgroundImage={bannerBackground}
+            />
+            <Galeria />
+          </ConteudoGaleria>
         </MainContainer>
       </AppContainer>
     </FundoGradiente>
---
+++ src/components/Galeria/index.jsx
@@ -0,0 +1,9 @@
+import Titulo from "../Titulo";
+
+const Galeria = () => {
+    return (
+        <Titulo>Navegue pela galeria</Titulo>
+    )
+}
+
+export default Galeria;
